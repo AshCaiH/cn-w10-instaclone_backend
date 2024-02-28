6d22ace1ff20a5de8ce894659d0e1cce599a98aa
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-28 00:00:59 +0000

    feature: working like system

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,5 +1,5 @@
 const { Image } = require("../models");
-const { User } = require("../models");
+const { Like } = require("../models");
 const { sendMessage, sendError } = require("../common/responses")
 
 module.exports.
@@ -18,8 +18,18 @@ like = async (req, res, next) => {
             })
         }
 
-        User.update( {UserId: req.authorisation.id}, {where: {id: req.body.id}});
 
-        sendMessage(res, "You liked the image", {image}, 201);
+        const likeOptions = { ImageId: image.id, UserId: req.authorisation.id}
+
+        let like = await Like.findOne({where: likeOptions});
+
+        // Add to the likes list if it hasn't been already.
+        if (!like) {
+            like = await Like.create(likeOptions);
+            sendMessage(res, "You liked the image", {image, like}, 201);
+        } else {
+            sendMessage(res, "You already liked the image", {image, like}, 201);
+        }
+
     } catch (error) {sendError(req, res, error);}
 }
